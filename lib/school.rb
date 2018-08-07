056218540a3c2429d0662d4fd2e2c47cb2452a99
Sav Younts <savyounts@gmail.com>
2018-08-07 17:47:07 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,20 @@
-# code here!
+class School
+  def initialize(name)
+    @roster = {}
+    @name = name
+  end
+  
+  def add_student(name, grade)
+    if @roster[grade] = nil 
+      @roster[grade] = []
+    end
+    @roster[grade] << name
+  end
+  
+  def grade(grade)
+    @roster[grade]
+  end
+  
+  
+  
+end
